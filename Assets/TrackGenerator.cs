8cf9d670c4148a2653ec0da22d2ac61e7d65955d
Aleksi Tella <aleksi.tella@gmail.com>
2019-03-09 13:59:56 +0200

    improve track generation, add uv map generation

--- Assets/TrackGenerator.cs
+++ Assets/TrackGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using MathNet.Numerics.Random;
 using MathNet.Spatial.Euclidean;
 using UnityEngine;
@@ -9,18 +10,25 @@ public class TrackGenerator : MonoBehaviour
 {
     //Has to be at least 4 points
     public Vector3[] controlPointsList;
-    public Transform track_tile;
+    public float trackWidth = 5f;
     SystemRandomSource rndsrc = new SystemRandomSource(12345, false);
     //Are we making a line or a loop?
     public bool isLooping = true;
+    public Color roadColor;
+    public Color lineColor;
+
     List<Vector3> vertices = new List<Vector3>();
+    List<Vector2> uvcoords = new List<Vector2>();
     List<int> tris = new List<int>();
     //Display without having to press play
+    long frameNum = 0;
+
     void OnDrawGizmos()
     {
         //Draw the Catmull-Rom spline between the points
         for (int i = 0; i < controlPointsList.Length; i++)
         {
+            Gizmos.DrawSphere(controlPointsList[i], 1f);
             //Cant draw between the endpoints
             //Neither do we need to draw from the second to the last endpoint
             //...if we are not making a looping line
@@ -53,15 +61,54 @@ public class TrackGenerator : MonoBehaviour
             vertices.MoveNext();
         }
 
+        // Move track to (0, 0, 0)
         Vector3 translate = Vector3.zero - vecs[0];
         for (int i = 0; i < vecs.Length; i++) {
-            vecs[i] += translate + new Vector3(0f, 0f, 10f);
+            vecs[i] += translate;
         }
         controlPointsList = vecs;
     }
-    void Start() 
+
+    public void RandomizeTexture(int seed) {
+        int textureSize = 500;
+        int lineWidth = textureSize / 10;
+        var texture = new Texture2D(textureSize, textureSize, TextureFormat.ARGB32, false);
+        double[] noise = SystemRandomSource.Doubles(textureSize * textureSize * 3, seed);
+        double[] colors = SystemRandomSource.Doubles(2 * 3, seed+1);
+        roadColor = new Color((float) colors[0], (float) colors[1], (float) colors[2], 1f);
+        lineColor = new Color((float) colors[3], (float) colors[4], (float) colors[5], 1f);
+
+        // set the pixel values
+        texture.SetPixels(Enumerable.Repeat<Color>(roadColor, textureSize*textureSize).ToArray());
+        for (int x = 0; x < lineWidth; x++) {
+            for (int y = 0; y < textureSize; y++) {
+                texture.SetPixel(x, y, lineColor);
+                texture.SetPixel(textureSize-x, y, lineColor);
+            }
+        }
+        for (int x = 0; x < textureSize; x++) {
+            for (int y = 0; y < textureSize; y++) {
+                int idx = y * textureSize * 3 + x;
+                Color noiseColor = new Color((float)noise[idx], (float)noise[idx+1], (float)noise[idx+2], 1f);
+                texture.SetPixel(x, y, texture.GetPixel(x, y) + (noiseColor * new Color(0.5f, 0.5f, 0.5f, 1f)));
+            }
+        }
+
+        // Apply all SetPixel calls
+        texture.Apply();
+        GetComponent<Renderer>().material.mainTexture = texture;
+    }
+    void OnRenderObject() {
+        if (frameNum % 100 == 0) {
+            RandomizeTexture(((int) (rndsrc.NextDoubles(1)[0] * 10000)));
+        }
+        frameNum += 1;
+    }
+    void Start()
     {
         if (controlPointsList == null) SetPoints();
+        RandomizeTexture(((int) (rndsrc.NextDoubles(1)[0] * 10000)));
+        // Create a new 2x2 texture ARGB32 (32 bit with alpha) and no mipmaps
         //Draw the Catmull-Rom spline between the points
         for (int i = 0; i < controlPointsList.Length; i++)
         {
@@ -86,11 +133,7 @@ public class TrackGenerator : MonoBehaviour
         triangles.Add(vertices.Count-2); triangles.Add(0); triangles.Add(vertices.Count - 1);
         triangles.Add(0); triangles.Add(1); triangles.Add(vertices.Count-1);
         mesh.triangles = triangles.ToArray();
-        Vector2[] uvs = new Vector2[vertices.Count];
-        for (int i = 0; i < uvs.Length; i++) {
-            uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
-        }
-        mesh.uv = uvs;
+        mesh.uv = uvcoords.ToArray();
         GetComponent<MeshCollider>().sharedMesh = mesh;
     }
 
@@ -105,10 +148,12 @@ public class TrackGenerator : MonoBehaviour
 
         //The start position of the line
         Vector3 lastPos = p1;
+        Vector3 lastd;
+        Vector3 lastp;
 
         //The spline's resolution
         //Make sure it's is adding up to 1, so 0.3 will give a gap, but 0.2 will work
-        float resolution = 0.05f;
+        float resolution = 0.01f;
 
         //How many times should we loop?
         int loops = Mathf.FloorToInt(1f / resolution);
@@ -119,21 +164,32 @@ public class TrackGenerator : MonoBehaviour
             float t = i * resolution;
 
             //Find the coordinate between the end points with a Catmull-Rom spline
-            Vector3[] catmullPosition = GetCatmullRomPosition(t, p0, p1, p2, p3);
-            Vector3 newPosition = catmullPosition[0];
-            Vector3 newd = catmullPosition[1];// / catmullPosition[1].magnitude;
-            Vector3 newDirection = Vector3.Normalize(newd);
-            Vector3 positionPlusDirection = newPosition + newDirection;
-            Vector3 p = new Vector3(newDirection.z, newDirection.y, -newDirection.x) * 5.0f;
+            Vector3[] catpos = GetCatmullRomPosition(t, p0, p1, p2, p3);
+            Vector3 newPos = catpos[0];
+            Vector3 newd = catpos[1];
+            Vector3 dir = Vector3.Normalize(newPos - lastPos);
+            Vector3 p = new Vector3(dir.z, dir.y, -dir.x) * trackWidth;
             //Draw this line segment
             if (editor) {
-                Gizmos.DrawLine(newPosition, positionPlusDirection);
-                Gizmos.DrawLine(newPosition, newPosition+p);
-                Gizmos.DrawLine(newPosition, newPosition-p);
+                Gizmos.DrawLine(lastPos, newPos);
+                Gizmos.DrawLine(newPos, newPos+p);
+                Gizmos.DrawLine(newPos, newPos-p);
             } else {
-                vertices.Add(newPosition-p);
-                vertices.Add(newPosition+p);
+                Vector3 uvdir = newPos - lastPos;
+                Vector3 uvp = new Vector3(uvdir.z, uvdir.y, -uvdir.x) * trackWidth;
+                Vector3 uv_zero = lastPos + uvp;
+                Vector3 uv_one = lastPos - uvp;
+                float segmentLength = uvdir.magnitude;
+                float segmentWidth = trackWidth * 2f;
+                Vector2 uv_1 = new Vector2(0f, (float)(i%2) * (segmentLength / segmentWidth));
+                Vector2 uv_2 = new Vector2(1f, (float)(i%2) * (segmentLength / segmentWidth));
+                uvcoords.Add(uv_1);
+                uvcoords.Add(uv_2);
+                vertices.Add(newPos-p);
+                vertices.Add(newPos+p);
             }
+            lastd = newd;
+            lastp = p;
 
             //Save this pos so we can draw the next line segment
             lastPos = newPosition;
