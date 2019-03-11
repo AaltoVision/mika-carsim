e3bf9af12c75e3a9db4be3a89517e6a8418fbed6
Aleksi Tella <aleksi.tella@gmail.com>
2019-03-11 18:15:59 +0200

    add skybox randomization

---
+++ Assets/CarSim/Scripts/RandomObjects.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CarSim.Randomization;
+using MathNet.Numerics.Random;
+
+public class RandomObjects : MonoBehaviour, IRandomizable
+{
+    void DestroyObject(GameObject obj) {
+        #if UNITY_EDITOR
+        DestroyImmediate(obj);
+        #else
+        Destroy(obj);
+        #endif
+    }
+
+    List<GameObject> children = new List<GameObject>();
+
+    void DestroyChildren() {
+        children.ForEach(child => DestroyObject(child));
+        children.Clear();
+    }
+
+    void GenerateChildren(SystemRandomSource rnd) {
+        for (int i = 0; i < 100; i++) {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.parent = transform;
+            double[] pos = rnd.NextDoubles(3);
+            cube.transform.position = new Vector3((float) pos[0] * 900f - 600f,
+                                                  (float) pos[1] * 5f -2.5f,
+                                                  (float) pos[2] * 900f - 450f + 150f);
+
+            double[] scale = rnd.NextDoubles(3);
+            cube.transform.localScale = new Vector3(1f + (float) scale[0] * 5f,
+                                                    1f + (float) scale[1] * 5f,
+                                                    1f + (float) scale[2] * 5f);
+
+            double[] rot = rnd.NextDoubles(3);
+            cube.transform.eulerAngles = new Vector3((float) rot[0] * 180,
+                                                     (float) rot[1] * 180,
+                                                     (float) rot[2] * 180);
+            double[] color = rnd.NextDoubles(3);
+            cube.GetComponent<Renderer>().material.color = new Color((float) color[0],
+                                                                           (float) color[1],
+                                                                           (float) color[2],
+                                                                           1f);
+            children.Add(cube);
+        }
+    }
+
+    public void Randomize(SystemRandomSource rnd) {
+        DestroyChildren();
+        GenerateChildren(rnd);
+    }
+}
---
+++ Assets/CarSim/Scripts/RandomSkybox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CarSim.Randomization;
+using MathNet.Numerics.Random;
+
+public class RandomSkybox : MonoBehaviour, IRandomizable
+{
+    public void Randomize(SystemRandomSource rnd) {
+        double[] color = rnd.NextDoubles(3);
+        GetComponent<Skybox>().material.SetColor("_SkyTint", new Color((float) color[0],
+                                                          (float) color[1],
+                                                          (float) color[2],
+                                                          1f));
+    }
+}
