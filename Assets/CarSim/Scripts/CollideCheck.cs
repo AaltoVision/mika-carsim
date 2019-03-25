99af939fcc04c515cd030284c766538d87b00b61
Aleksi Tella <aleksi.tella@gmail.com>
2019-03-25 10:53:58 +0200

    fix reset logic

--- Assets/CarSim/Scripts/CollideCheck.cs
+++ Assets/CarSim/Scripts/CollideCheck.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class CollideCheck : MonoBehaviour
 {
     public TemplateAgent agent;
+    public GameObject track;
+    public GameObject ground;
 
+    private bool isOnTrack = false;
     void Awake()
     {
         BoxCollider collider = GetComponent<BoxCollider>();
         collider.isTrigger = true;
     }
 
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject == track) {
+                Debug.Log("Track Enter");
+                isOnTrack = true;
+        } else if (collider.gameObject == ground)
+                Debug.Log("Ground Enter");
+    }
     private void OnTriggerExit(Collider collider)
     {
-        agent.OnCrash();
+        if (isOnTrack) {
+            isOnTrack = false;
+            agent.OnCrash();
+        }
     }
 }
